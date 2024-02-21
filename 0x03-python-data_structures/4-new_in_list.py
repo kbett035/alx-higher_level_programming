622b0f8f9c90e5251407495ee812025b5c7f384e
wyclife <81298471+kbett035@users.noreply.github.com>
2024-02-21 13:32:40 +0300

    4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-# 4-new_in_list.py
-# Gedeon Obae Gekonge <gideonobae@gmail.com.com>
+# 4-new_in_list.py task
+# Wyclife Kimutai <kimutaiwyclife6@gmail.com
 
 
 def new_in_list(my_list, idx, element):
