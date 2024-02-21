c7f94803132983969fa9601174eb25eea43681d0
wyclife <81298471+kbett035@users.noreply.github.com>
2024-02-21 12:38:34 +0300

    Update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 # 6-print_comb3.py
-# Gedeon Obae Gekonge <gideonobae@gmail.com>
-
+# Wyclife Kimutai <kimutaiwyclife6@gmail.com>
 """Print all possible different combinations of two digits in ascending order.
     The two digits must be different - 01 and 10 are considered identical.
     """
