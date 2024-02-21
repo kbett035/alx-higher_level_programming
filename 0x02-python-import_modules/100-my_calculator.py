e4a1bdf7a894c06b7e94194f00b2b95d73bc749a
wyclife <81298471+kbett035@users.noreply.github.com>
2024-02-21 13:03:35 +0300

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-# 100-my_calculator.py
-# Gedeon Obae Gekonge <gideonobae@gmail.com>
+# Wyclife Kimutai <kimutaiwyclife6@gmail.com
+# 100-my_calculator.py task
 
 if __name__ == "__main__":
     """Handle basic arithmetic operations."""
