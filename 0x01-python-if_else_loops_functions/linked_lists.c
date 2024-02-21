8c56f530f41d7049fd36714602f2eb507f8e365c
wyclife <81298471+kbett035@users.noreply.github.com>
2024-02-21 12:41:09 +0300

    Update linked_lists.c

--- 0x01-python-if_else_loops_functions/linked_lists.c
+++ 0x01-python-if_else_loops_functions/linked_lists.c
@@ -3,9 +3,9 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all elements of a listint_t list
- * @h: pointer to head of list
- * Return: number of nodes
+ * print_listint - print all elements of a listint_t list
+ * @h: pointers to head of list
+ * Return: number of node
  */
 size_t print_listint(const listint_t *h)
 {
