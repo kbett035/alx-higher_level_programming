ab0e560d5759e669d2c66a6e8cb09969301264fb
wyclife <81298471+kbett035@users.noreply.github.com>
2024-02-21 13:31:54 +0300

    2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # 2-replace_in_list.py
-# Gedeon Obae Gekonge <gideonobae@gmail.com>
+# Wyclife Kimutai <kimutaiwyclife6@gmail.com
 
 
 def replace_in_list(my_list, idx, element):
