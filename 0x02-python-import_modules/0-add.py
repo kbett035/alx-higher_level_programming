2fda836c3742676610e03f5db2b7afc093784a5a
wyclife <81298471+kbett035@users.noreply.github.com>
2024-02-21 13:02:33 +0300

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
+# Wyclife Kimutai <kimutaiwyclife6@gmail.com
 # 0_add.py
-# Gedeon Obae Gekonge <gideonobae@gmail.com>
 
 if __name__ == "__main__":
     """Print the sum of 1 and 2."""
