36cffd8c1a32985e8229319c45cda4274959a39f
wyclife <81298471+kbett035@users.noreply.github.com>
2024-02-21 13:06:12 +0300

    5-variable_load.py

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-# 5-variable_load.py
-# Gedeon Obae Gekonge <gideonobae@gmail.com.com>
-
+# 5-variable_load.py task
+# Wyclife Kimutai <kimutaiwyclife6@gmail.com
 if __name__ == "__main__":
     """Print the value of variable a from variable_load_5."""
     from variable_load_5 import a
