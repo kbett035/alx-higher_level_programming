c6ec6ae8a51e208c0c123a67d00fa6888ffd5814
wyclife <81298471+kbett035@users.noreply.github.com>
2024-02-24 20:58:39 +0300

    3-common_elements.py

--- 0x08-python-more_classes/3-common_elements.py
+++ 0x08-python-more_classes/3-common_elements.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 # 3-common_elements.py
-# Gedeon Obae Gekonge <gideonobae@gmail.com>
+# Wyclife Kimutai <kimutaiwyclife6@gmail.com>
 
 
 def common_elements(set_1, set_2):
-    """Return a set of common elements in two sets."""
+    """Returns set of common elements in two sets."""
     return (set_1 & set_2)
