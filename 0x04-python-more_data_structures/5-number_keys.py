de56d0a4531ef8631d3fa06573866d4fa02df658
wyclife <81298471+kbett035@users.noreply.github.com>
2024-02-24 11:46:57 +0300

    5-number_keys.py

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
+# Wyclife Kimutai <kimutaiwyclife6@gmail.com>
 # 5-number_keys.py
-# Gedeon Obae Gekonge <gideonobae@gmail.com>
-
 
 def number_keys(a_dictionary):
-    """Return the number of keys in a dictionary."""
+    """Returns number of keys in a dictionary."""
     return (len(a_dictionary))
