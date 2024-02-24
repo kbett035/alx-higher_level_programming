82d88486edfcbd8c64db3dbe1585f9a525f5db8b
wyclife <81298471+kbett035@users.noreply.github.com>
2024-02-24 11:50:12 +0300

    9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 # 9-multiple_by_2.py
-# Gedeon Obae Gekonge <gideonobae@gmail.com>
 
 
 def multiply_by_2(a_dictionary):
-    """Return a new dictionary with all values multipled by 2."""
+    """Returns a new dictionary with all values multipled by two."""
     return ({k: a_dictionary[k] * 2 for k in a_dictionary})
