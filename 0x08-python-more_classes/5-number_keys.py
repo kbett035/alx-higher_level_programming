a7724faf2fdbbb73aeca02f2ca2076bec7e80df6
wyclife <81298471+kbett035@users.noreply.github.com>
2024-02-24 21:00:12 +0300

    5-number_keys.py

--- 0x08-python-more_classes/5-number_keys.py
+++ 0x08-python-more_classes/5-number_keys.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 # 5-number_keys.py
-# Gedeon Obae Gekonge <gideonobae@gmail.com>
+# Wyclife Kimutai <kimutaiwyclife6@gmail.com>
 
 
 def number_keys(a_dictionary):
-    """Return the number of keys in a dictionary."""
+    """Returns a number of keys in a dictionary."""
     return (len(a_dictionary))
