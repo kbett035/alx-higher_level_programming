628848c16a1266c89b2e3158b20842d52cec4ed0
wyclife <81298471+kbett035@users.noreply.github.com>
2024-02-24 21:01:16 +0300

    7-update_dictionary.py

--- 0x08-python-more_classes/7-update_dictionary.py
+++ 0x08-python-more_classes/7-update_dictionary.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+# Wyclife Kimutai <kimutaiwyclife6@gmail.com>
 # 7-update_dictionary.py
-# Gedeon Obae Gekonge <gideonobae@gmail.com>
+
 
 
 def update_dictionary(a_dictionary, key, value):
-    """Replace or add key/value pairs in a dictionary."""
+    """Replaces or add key/value pair in a dictionary."""
     a_dictionary[key] = value
     return (a_dictionary)
