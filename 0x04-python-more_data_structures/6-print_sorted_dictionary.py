6498f77161e268bcc28cffda7daac35cb584e045
wyclife <81298471+kbett035@users.noreply.github.com>
2024-02-24 11:47:47 +0300

    Updated 6-print_sorted_dictionary.py

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+# Wyclife Kimutai <kimutaiwyclife6@gmail.com>
 # 6-print_sorted_dictionary.py
-# Gedeon Obae Gekonge <gideonobae@gmail.com>
 
 
 def print_sorted_dictionary(a_dictionary):
-    """Print a dictionary by ordered keys."""
+    """Prints dictionary by ordered keys"""
     [print("{}: {}".format(k, a_dictionary[k])) for k in sorted(a_dictionary)]
