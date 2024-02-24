0cd885b95e829ad62d6969765489178f89375fa6
wyclife <81298471+kbett035@users.noreply.github.com>
2024-02-24 12:36:36 +0300

    5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 # 5-square.py
-# Gedeon Obae Gekonge <gideonobae@gmail.com.com>
+# Wyclife Kimutai <kimutaiwyclife6@gmail.com>
 """Define a class Square."""
 
 
 class Square:
-    """Represent a square."""
+    """Represents a square."""
 
     def __init__(self, size):
         """Initialize a new square.
