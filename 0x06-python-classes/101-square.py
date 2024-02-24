4f876a8a3e4ac8c1669b8dc4d02531576c0dc1cd
wyclife <81298471+kbett035@users.noreply.github.com>
2024-02-24 12:32:42 +0300

    101-square.py

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 # 101-square.py
-# Geeon Obae Gekonge <gideonobae@gmail.com>
+# Wyclife Kimutai <kimutaiwyclife6@gmail.com>
 """Define a class Square."""
 
 
 class Square:
-    """Represent a square."""
+    """Represents a square."""
 
     def __init__(self, size=0, position=(0, 0)):
         """Initialize a new square.
