4b060630f419e52249b92ebfe4c64e33c750a8b9
wyclife <81298471+kbett035@users.noreply.github.com>
2024-02-24 11:44:41 +0300

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 # 12-roman_to_int.py
-# Brennan D Baraban <375@holbertonschool.com>
+# Wyclife Kimutai <kimutaiwyclife6@gmail.com>
 
 
 def roman_to_int(roman_string):
-    """Converts a roman numeral to an integer."""
+    """It Convert a roman numeral to an integer"""
     if (not isinstance(roman_string, str) or
             roman_string is None):
         return (0)
